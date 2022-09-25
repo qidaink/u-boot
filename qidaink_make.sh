39686e3c6e90d1c4172aaff218860ca6e3e28e3e
qidaink <2038035593@qq.com>
2022-09-25 17:52:56 +0800

    feat:添加编译脚本文件和烧写工具
    - 添加编译脚本
    - 添加烧写u-boot.bin文件到SD卡的工具
    - 首次编译整个工程

---
+++ qidaink_make.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# =====================================================
+# Copyright © hk. 2022-2025. All rights reserved.
+# File name: qidaink_make.sh
+# Author   : qidaink
+# Date     : 2022-08-13
+# Version  : 
+# Description: 编译命令脚本文件
+# Others   : 
+# Log      : 
+# ======================================================
+##
+
+# ======================================================
+## 编译 NXP 官方开发板对应的 uboot
+echo "\033[1;32m[INFO]Cleaning up project files ... \033[0m"
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+echo "\033[1;32m[INFO]Configuring the Project ... \033[0m"
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_14x14_evk_emmc_defconfig 
+echo "\033[1;32m[INFO]Building project in progress ... \033[0m"
+make V=0 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j16
+echo "\033[1;32m[INFO]Compile the complete!!!\033[0m"
